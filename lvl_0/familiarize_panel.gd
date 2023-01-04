866e3b00ffa389c3bc155d5920933f95821c2dcd
Isaiah Raspet <isaiahraspet@gmail.com>
2023-01-04 15:56:42 -0700

    level 0 pt 1

--- lvl_0/familiarize_panel.gd
+++ lvl_0/familiarize_panel.gd
@@ -6,6 +6,7 @@ onready var send_zero = $send_zero
 onready var send_one = $send_one
 onready var rec_blue = $rec_blue
 onready var rec_green = $rec_green
+onready var result = $result
 
 enum COLOR_STATE {
 	BLUE,
@@ -34,7 +35,16 @@ func _input(_event):
 		send_number_selected = false
 		rec_color_selected = false
 	if toggle:
-		print( "toggle toggled" )
+		randomize()
+		if send_color_state == rec_color_state:
+			if send_number_state == NUMBER_STATE.ZERO:
+				result.text = "0"
+			else:
+				result.text = "1"
+		else:
+			var temp = rand_range(0, 2)
+			temp = floor(temp)
+			result.text = String(temp)
 		toggle = false
 
 func _on_send_blue_pressed():
@@ -42,47 +52,70 @@ func _on_send_blue_pressed():
 	send_green.disabled = true
 	send_color_state = COLOR_STATE.BLUE
 	send_color_selected = true
+	send_green.hide()
 
 func _on_send_green_pressed():
 	send_blue.disabled = true
 	send_green.disabled = true
 	send_color_state = COLOR_STATE.GREEN
 	send_color_selected = true
+	send_blue.hide()
 
 func _on_send_zero_pressed():
 	send_zero.disabled = true
 	send_one.disabled = true
 	send_number_state = NUMBER_STATE.ZERO
 	send_number_selected = true
+	send_one.hide()
 
 func _on_send_one_pressed():
 	send_zero.disabled = true
 	send_one.disabled = true
 	send_number_state = NUMBER_STATE.ONE
 	send_number_selected = true
+	send_zero.hide()
 
 func _on_reset_btn_pressed():
 	send_blue.disabled = false
+	send_blue.show()
+	
 	send_green.disabled = false
+	send_green.show()
+	
 	send_zero.disabled = false
+	send_zero.show()
+	
 	send_one.disabled = false
+	send_one.show()
+	
 	rec_blue.disabled = false
+	rec_blue.show()
+	
 	rec_green.disabled = false
+	rec_green.show()
+	
 	send_color_state = COLOR_STATE.NONE
 	send_number_state = NUMBER_STATE.NONE
 	rec_color_state = COLOR_STATE.NONE
+	
 	send_color_selected = false
 	send_number_selected = false
 	rec_color_selected = false
+	
+	result.text = ""
 
 func _on_rec_blue_pressed():
 	rec_blue.disabled = true
 	rec_green.disabled = true
 	rec_color_state = COLOR_STATE.BLUE
 	rec_color_selected = true
+	rec_green.hide()
+
 
 func _on_rec_green_pressed():
 	rec_blue.disabled = true
 	rec_green.disabled = true
 	rec_color_state = COLOR_STATE.GREEN
 	rec_color_selected = true
+	rec_blue.hide()
+
