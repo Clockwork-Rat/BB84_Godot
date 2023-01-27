a0b2f819fbd01d389d73cd97b5faa818801b2e23
Isaiah Raspet <isaiahraspet@gmail.com>
2023-01-27 10:42:44 -0700

    continued update

--- main_menu/scene_assets/mm_bg_camera.gd
+++ main_menu/scene_assets/mm_bg_camera.gd
@@ -10,7 +10,7 @@ onready var animation_player = $"../main_menu_anim_player"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animation_player.play("camera_bob")
+	animation_player.play("camera_bob", -1, 0.2, false)
 	pass
 
 
